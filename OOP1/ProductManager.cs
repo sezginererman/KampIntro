ec53a53a5ee0198ec44d52910febd70e83a161b0
Sezgin Ererman <sezginererman@gmail.com>
2021-04-08 00:05:21 +0300

    4. gün 2 ders saat sonu.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        //void ne demek?
+        //işlem bittiğinde ne olduğunu bilmeme gerek yoksa void kullanabilirsin
+        //ama sana yapılan işlem sonucu sana lazımsa /return kullanman lazım.
+
+        public void Add(Product product) 
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");   
+        }
+
+        public void Update(Product product) 
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+
+    }
+}

d8f6223067923e1a7c32c22c033179fbaa128fbd
Sezgin Ererman <sezginererman@gmail.com>
2021-04-14 22:30:36 +0300

    4. ders sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = MyList<string>();
+            isimler.Add("Sezgin");
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
